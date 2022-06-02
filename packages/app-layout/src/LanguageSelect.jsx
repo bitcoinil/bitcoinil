aaef99d7316c87568251fab6a95f139c4f1b5e3c
Rick <rickbgove@gmail.com>
2022-06-02 07:08:13 +0200

    Create Language Select Comp

---
+++ packages/app-layout/src/LanguageSelect.jsx
@@ -0,0 +1,44 @@
+import { Select } from "antd"
+import React from "react"
+import { FormattedMessage, IntlProvider } from "react-intl"
+import styled from "styled-components"
+import { useIntl } from "./hooks/useIntl"
+
+export default function LanguageSelect() {
+  const intl = useIntl()
+  const { language, setLanguage, messages, availableLanguages } = intl
+
+  return (
+    <IntlProvider
+      messages={messages[language]}
+      locale={language}
+      defaultLocale="en"
+    >
+      <StyledLanguageSelect>
+        <Select
+          onChange={(e) => {
+            setLanguage(e)
+          }}
+          defaultValue="en"
+        >
+          {availableLanguages.map((avLang) => {
+            return (
+              <Select.Option key={`select-language-${avLang}`} value={avLang}>
+                {avLang}
+              </Select.Option>
+            )
+          })}
+        </Select>
+        <FormattedMessage
+          id="app.text"
+          defaultMessage="This is some text"
+          description="Link on react page"
+        />
+      </StyledLanguageSelect>
+    </IntlProvider>
+  )
+}
+
+const StyledLanguageSelect = styled.div`
+  height: 110px;
+`
