27bb25084155ad1263bad76f57b99b7071f42b2a
Rick <rickbgove@gmail.com>
2022-06-02 07:16:01 +0200

    change logo to todo: logo so we can find it easier

--- packages/app-layout/src/Header.jsx
+++ packages/app-layout/src/Header.jsx
@@ -15,7 +15,7 @@ export default function Header({ navigate }) {
             navigate("/")
           }}
         >
-          Logo
+          TODO: Logo
         </p>
       </div>
       <div className="middle">
