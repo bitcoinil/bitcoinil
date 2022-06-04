5dc2838bcbe79f64ec1ba7cd02af37512dd1df68
Rick <rickbgove@gmail.com>
2022-06-04 14:32:41 +0100

    convert dictionaries to ts

--- apps/bitcoin-il/src/translations/he.js
+++
@@ -1,4 +0,0 @@
-export const messages_he = {
-  'app.text': 'זהו טקסט כלשהו בשפה שנבחרה',
-  'page.individuals.title': 'יחידים'
-}
---
+++ apps/bitcoin-il/src/translations/he.ts
@@ -0,0 +1,9 @@
+export const messages_he = {
+  'app.text': 'זהו טקסט כלשהו בשפה שנבחרה',
+  'page.individuals.title': 'יחידים',
+  'page.developers.title': 'Translation Needed',
+  'page.getting-started.title': 'Translation Needed',
+  'page.how-it-works.title': 'Translation Needed',
+  'page.white-paper.title': 'Translation Needed',
+  'page.innovation.title': 'Translation Needed'
+}
