03470f734f7dfa6d5c9ee4a1c52a6aa7e1f89468
Rick <rickbgove@gmail.com>
2022-06-06 13:29:12 +0200

    Fix ts-ingore on HeaderMenu

--- apps/bitcoin-il/src/Header.tsx
+++ apps/bitcoin-il/src/Header.tsx
@@ -1,9 +1,7 @@
 import * as React from 'react'
 import { useNavigate } from 'react-router-dom'
 import styled from 'styled-components'
-// @ts-ignore
 import HeaderMenu from './HeaderMenu'
-// @ts-ignore
 import LanguageSelect from './LanguageSelect'
 
 export default function Header(): JSX.Element {
