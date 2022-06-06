ecb56c20e4451c7f01b96d83157e5dc7bd5f41f9
Rick <rickbgove@gmail.com>
2022-06-06 13:28:53 +0200

    Init DevTools

---
+++ apps/bitcoin-il/src/DevTools.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react'
+
+import styled from 'styled-components'
+
+export default function DevTools() {
+  return (
+    <StyledDevTools>
+      <h1>DEV TOOLS</h1>
+      <button onClick={() => console.clear()}>Clear Console</button>
+    </StyledDevTools>
+  )
+}
+
+const StyledDevTools = styled.div`
+  position: absolute;
+  top: 50vh;
+  right: 0;
+  border: 2px solid;
+  padding: 25px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+
+  button {
+    font-size: 18px;
+    padding: 20px;
+    margin-bottom: 10px;
+  }
+`
