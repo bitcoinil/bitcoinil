7dd89dc20980018043b300bd4bf869283265baf1
Rick <rickbgove@gmail.com>
2022-06-06 15:37:49 +0200

    fix ts error

--- apps/bitcoin-il/src/HeaderMenu.tsx
+++ apps/bitcoin-il/src/HeaderMenu.tsx
@@ -1,13 +1,10 @@
 import { Menu } from 'antd'
 import * as React from 'react'
-import styled from 'styled-components'
 import { useNavigate } from 'react-router-dom'
-
+import styled from 'styled-components'
 import Arrow from './img/ico_angle.svg'
-
-//@ts-ignore
-import { mainMenuItems } from './mainMenuItems'
 import { ClickedMenuItemData, MainMenuItem } from './Interfaces'
+import { mainMenuItems } from './mainMenuItems'
 
 export default function HeaderMenu(): JSX.Element {
   const [current, setCurrent] = React.useState('mail')
@@ -20,71 +17,74 @@ export default function HeaderMenu(): JSX.Element {
   }
 
   return (
-    <StyledAppMenu onClick={onClick} selectedKeys={[current]} mode="horizontal">
-      {mainMenuItems.map((item: MainMenuItem) => {
-        console.log('🌠', item)
-        if (item.submenu) {
+    <StyledAppMenu>
+      <Menu onClick={() => onClick} selectedKeys={[current]} mode="horizontal">
+        {mainMenuItems.map((item: any) => {
+          if (item.submenu) {
+            return (
+              <Menu.SubMenu
+                key={item.key}
+                title={
+                  <p className="collapsable-menu">
+                    {item.label} <img className="menu-arrow" src={Arrow} />
+                  </p>
+                }
+              >
+                {item.submenu.map((subItem: MainMenuItem) => {
+                  return (
+                    <Menu.Item key={subItem.key}>{subItem.label}</Menu.Item>
+                  )
+                })}
+              </Menu.SubMenu>
+            )
+          }
           return (
-            <Menu.SubMenu
-              key={item.key}
-              title={
-                <p className="collapsable-menu">
-                  {item.label} <img className="menu-arrow" src={Arrow} />
-                </p>
-              }
-            >
-              {item.submenu.map((subItem: MainMenuItem) => {
-                return <Menu.Item key={subItem.key}>{subItem.label}</Menu.Item>
-              })}
-            </Menu.SubMenu>
+            <Menu.Item key={item.key} onClick={() => console.log(item)}>
+              <p>{item.label}</p>
+            </Menu.Item>
           )
-        }
-        return (
-          <Menu.Item key={item.key} onClick={() => console.log(item)}>
-            <p>{item.label}</p>
-          </Menu.Item>
-        )
-      })}
+        })}
+      </Menu>
     </StyledAppMenu>
   )
 }
 
-// @ts-ignore
-const StyledAppMenu = styled(Menu)`
-  width: 80vw;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+export const StyledAppMenu = styled.div`
+  .ant-menu {
+    width: 80vw;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    &.ant-menu {
+      background: transparent;
+      border-bottom: none;
 
-  &.ant-menu {
-    background: transparent;
-    border-bottom: none;
+      p {
+        margin: 0;
+      }
 
-    p {
-      margin: 0;
-    }
-
-    .menu-arrow {
-      transition: all 200ms;
-    }
-
-    .collapsable-menu {
-      transition: all 200ms;
-      &:hover > .menu-arrow {
+      .menu-arrow {
         transition: all 200ms;
-        transform: rotate(180deg);
       }
-    }
 
-    > li {
-      &::after {
-        width: 0;
-        height: 0;
+      .collapsable-menu {
+        transition: all 200ms;
+        &:hover > .menu-arrow {
+          transition: all 200ms;
+          transform: rotate(180deg);
+        }
       }
-      &.ant-menu-item {
+
+      > li {
         &::after {
           width: 0;
           height: 0;
+        }
+        &.ant-menu-item {
+          &::after {
+            width: 0;
+            height: 0;
+          }
         }
       }
     }
