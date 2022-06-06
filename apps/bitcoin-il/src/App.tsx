66028fe812ed2aa35a57c322500930d3bcf101c5
Rick <rickbgove@gmail.com>
2022-06-06 15:37:37 +0200

    extract renderroutes

--- apps/bitcoin-il/src/App.tsx
+++ apps/bitcoin-il/src/App.tsx
@@ -2,52 +2,18 @@ import '@djitsu/themes/dist/themes/main-theme/main-theme-main-light.css'
 import 'antd/dist/antd.css' // or 'antd/dist/antd.less'
 import * as React from 'react'
 import { IntlProvider } from 'react-intl'
-import { Route, Routes } from 'react-router-dom'
+import RenderRoutes from './RenderRoutes'
 import DevTools from './DevTools'
 import Header from './Header'
 // import { useNavigate } from 'react-router-dom'
 import { useIntl } from './hooks/useIntl'
-import { mainMenuItems } from './mainMenuItems'
 import Support from './support'
 
 function App(): JSX.Element {
-  const { language, messages, setLanguage, locale } = useIntl()
-
-  const renderRoutes = () => {
-    return (
-      <Routes>
-        {mainMenuItems.map((menuItem, i) => {
-          const { submenu } = menuItem
-
-          if (submenu) {
-            return submenu.map((subMenuItem, ii) => {
-              return (
-                <Route
-                  key={`submenu-item-${ii}`}
-                  path={`/${subMenuItem.key}`}
-                  element={subMenuItem.element}
-                />
-              )
-            })
-          }
-
-          return (
-            <Route
-              key={i}
-              path={`/${menuItem.key}`}
-              element={menuItem.element}
-            />
-          )
-        })}
-
-        <Route path="*" element={<Home />} />
-      </Routes>
-    )
-  }
+  const { language, messages, locale } = useIntl()
 
   return (
     <IntlProvider
-      // @ts-ignore
       messages={messages[language]}
       locale={locale}
       defaultLocale="en"
@@ -56,14 +22,10 @@ function App(): JSX.Element {
         <DevTools />
         <Support />
         <Header />
-        {renderRoutes()}
+        {RenderRoutes()}
       </div>
     </IntlProvider>
   )
 }
 
 export default App
-
-const Home = () => {
-  return <p>Home Page Here</p>
-}
