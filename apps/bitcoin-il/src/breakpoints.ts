8148e0edb858ca2224e9f6f76daa63a36e3585e8
Rick <rickbgove@gmail.com>
2022-06-08 13:14:49 +0200

    Add css breakpoints as consts

---
+++ apps/bitcoin-il/src/breakpoints.ts
@@ -0,0 +1,17 @@
+const phoneDevices: string = `@media only screen and (max-width: 600px)`
+
+const smallDevices: string = `@media only screen and (min-width: 600px)`
+
+const tabletDevices: string = `@media only screen and (min-width: 768px)`
+
+const largeDevices: string = `@media only screen and (min-width: 992px)`
+
+const extraLargeDevices: string = `@media only screen and (min-width: 1200px)`
+
+export {
+  phoneDevices,
+  tabletDevices,
+  largeDevices,
+  extraLargeDevices,
+  smallDevices
+}
