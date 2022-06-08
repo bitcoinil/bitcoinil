4b9c2e82fd7ffd43158f758e370a38bd44056dcb
Rick <rickbgove@gmail.com>
2022-06-08 14:42:27 +0200

    Revert "Comment out basname in vite config"
    This reverts commit 943ef2763ce21d13d80130639d8fdd15aa257e92.

--- apps/bitcoin-il/vite.config.ts
+++ apps/bitcoin-il/vite.config.ts
@@ -1,14 +1,14 @@
-import reactRefresh from '@vitejs/plugin-react-refresh'
-import { defineConfig } from 'vite'
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [reactRefresh()],
-  // base: '/bitcoinil.org/',
+  base: '/bitcoinil.org/',
   server: {
     hmr: {
-      protocol: 'ws',
-      host: 'localhost',
+      protocol: "ws",
+      host: "localhost",
       port: 39911
     }
   }
-})
+});
