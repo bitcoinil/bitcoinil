03ed3d8e49ae6c902add32f2c754c820884005a6
Rick <rickbgove@gmail.com>
2022-06-08 14:42:25 +0200

    Revert "Add basename to router"
    This reverts commit f5bc43cc9ecae64a28d2c7164ce65352be1b1990.

--- apps/bitcoin-il/src/main.tsx
+++ apps/bitcoin-il/src/main.tsx
@@ -4,7 +4,7 @@ import App from './App'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.render(
-  <Router basename={import.meta.env.BASE_URL}>
+  <Router>
     <React.StrictMode>
       <App />
     </React.StrictMode>
