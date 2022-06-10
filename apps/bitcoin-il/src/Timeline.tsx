89c94ea37d2db4f07b4c416584583f386a4f10a8
Rick <rickbgove@gmail.com>
2022-06-10 16:45:03 +0200

    Init timelinecomp

--- apps/bitcoin-il/src/Timeline.tsx
+++ apps/bitcoin-il/src/Timeline.tsx
@@ -1,21 +1,148 @@
-import { Timeline } from 'antd'
 import * as React from 'react'
-import { TimelineCompProps } from './Interfaces'
+import { FormattedMessage } from 'react-intl'
+import styled from 'styled-components'
+import {
+  VerticalTimeline,
+  VerticalTimelineElement
+} from 'react-vertical-timeline-component'
+import 'react-vertical-timeline-component/style.min.css'
+
+interface TimelineCompProps {
+  items: JSX.Element[]
+}
 
 const TimelineComp: React.FC<TimelineCompProps> = ({ items }) => {
   if (!items) return null
-
   return (
-    <Timeline mode="alternate">
-      {items.map((item: React.ReactNode, i: number) => {
-        return <Timeline.Item key={i}>{item}</Timeline.Item>
-      })}
-      {/* <Timeline.Item>Create a services site 2015-09-01</Timeline.Item>
-    <Timeline.Item>Solve initial network problems 2015-09-01</Timeline.Item>
-    <Timeline.Item>Technical testing 2015-09-01</Timeline.Item>
-    <Timeline.Item>Network problems being solved 2015-09-01</Timeline.Item> */}
-    </Timeline>
+    <StyledTimelineComp id="TimelineComp">
+      <VerticalTimeline>
+        {items.map((item: JSX.Element, i: number) => {
+          return (
+            <VerticalTimelineElement
+              icon={<h1 className="icon">{i + 1}</h1>}
+              key={i}
+            >
+              {item}
+            </VerticalTimelineElement>
+          )
+        })}
+        {/* <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+          date="2011 - present"
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          icon={<h1>1</h1>}
+        >
+          <h3 className="vertical-timeline-element-title">Creative Director</h3>
+          <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+          <p>
+            Creative Direction, User Experience, Visual Design, Project
+            Management, Team Leading
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2010 - 2011"
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          // icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">Art Director</h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            San Francisco, CA
+          </h4>
+          <p>
+            Creative Direction, User Experience, Visual Design, SEO, Online
+            Marketing
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2008 - 2010"
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          // icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">Web Designer</h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            Los Angeles, CA
+          </h4>
+          <p>User Experience, Visual Design</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2006 - 2008"
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          // icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">Web Designer</h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            San Francisco, CA
+          </h4>
+          <p>User Experience, Visual Design</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="April 2013"
+          iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+          // icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Content Marketing for Web, Mobile and Social Media
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
+          <p>Strategy, Social Media</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="November 2012"
+          iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+          // icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Agile Development Scrum Master
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">Certification</h4>
+          <p>Creative Direction, User Experience, Visual Design</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2002 - 2006"
+          iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+          // icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Bachelor of Science in Interactive Digital Media Visual Imaging
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            Bachelor Degree
+          </h4>
+          <p>Creative Direction, Visual Design</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
+          // icon={<StarIcon />}
+        /> */}
+      </VerticalTimeline>
+    </StyledTimelineComp>
   )
 }
 
 export default TimelineComp
+
+const StyledTimelineComp = styled.div`
+  width: 100vw;
+
+  .icon {
+  }
+
+  .vertical-timeline-element-icon {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    h1 {
+      margin: 0;
+      font-weight: bolder;
+    }
+  }
+`
