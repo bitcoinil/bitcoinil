1aa54e8588dd32bb65c34b34ccfd8ce687073a14
Rick <rickbgove@gmail.com>
2022-06-16 16:21:07 +0200

    mobile format for two column alternating info component

--- apps/bitcoin-il/src/AlternatingTwoColumnRow.tsx
+++ apps/bitcoin-il/src/AlternatingTwoColumnRow.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import styled from 'styled-components'
 import { AlternatingTwoColumnRowProps } from './Interfaces'
+import { phoneDevices } from './breakpoints'
 
 const AlternatingTwoColumnRow: React.FC<AlternatingTwoColumnRowProps> = ({
   index,
@@ -31,9 +32,23 @@ export default AlternatingTwoColumnRow
 const StyledAlternatingTwoColumnRow = styled.div`
   display: flex;
   padding: 65px;
+  margin-top: 50px;
+
+  ${phoneDevices} {
+    flex-direction: column;
+    padding: 0;
+
+    img {
+      max-width: 75vw;
+    }
+  }
 
   &.even {
     flex-direction: row-reverse;
+
+    ${phoneDevices} {
+      flex-direction: column;
+    }
   }
 
   &.odd {
